0ba937e11b87e48b2df0b08689c54b9e61e55215
Ethan <ethanrodkin@gmail.com>
2017-12-03 11:27:30 -0500

    Solution to 1b, cleaned both to prevent line going over 80 characters.

--- aoc1b.py
+++ aoc1b.py
@@ -1,4 +1,4 @@
 with open("aoc1.txt") as f:
     for line in f:
         print(sum(int(c) for i, c in enumerate(line) if c
-              == line[(i + 1) % len(line.strip())]))
+              == line[(i + (len(line.strip()) // 2)) % len(line.strip())]))
