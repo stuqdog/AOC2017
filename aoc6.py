c9bf87a1559d30eca97b03e51815a3e0da1cb2d1
Ethan <ethanrodkin@gmail.com>
2017-12-06 00:20:26 -0500

    Parts one and two.

---
+++ aoc6.py
@@ -0,0 +1,27 @@
+with open('aoc6.txt') as f:
+    for line in f:
+        layout = [int(x) for x in line.split(' ')]
+
+
+previous_positions = {}
+moves = 0
+
+
+while True:
+    val = max(layout)
+    length = len(layout)
+
+    start = layout.index(val)
+
+    i = 0
+    while layout[start] != 0:
+        layout[(start + i) % length] += 1
+        layout[start] -= 1
+        i += 1
+    if str(layout) in previous_positions:
+        break
+    moves += 1
+    previous_positions[str(layout)] = moves
+
+print("part 1: {}".format(moves + 1))
+print("part two: {}".format(moves + 1 - previous_positions[str(layout)]))
