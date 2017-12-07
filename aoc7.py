22c745e7da3ac6fdb351dbc2975e9312a08883b6
Ethan <ethanrodkin@gmail.com>
2017-12-07 11:02:01 -0500

    Saving for record. It's gotten too messy. Need to start over from line21

--- aoc7.py
+++ aoc7.py
@@ -3,8 +3,6 @@ from collections import Counter
 with open('aoc7.txt') as f:
     file_ = [line.split() for line in f]
 
-solution = []
-solution_two = 0
 structure = {}
 test = []
 
@@ -24,26 +22,27 @@ for program in file_:
 
 pillars = [x for x in structure[solution][1]]
 weight = []
+possible_solution = 0
 
-
-def create_pillars(base):
+def create_pillars(base, possible_solution):
     pillars = [x for x in structure[base][1]]
     weight = []
     for x in pillars:
         weight.append(0)
         weight[-1] += structure[x][0]
-        weight, possible_solution = add_weight(x, weight)
+        weight, possible_solution = add_weight(x, weight, possible_solution)
     print(pillars)
     return pillars, weight, possible_solution
 
-def add_weight(thing, weight_sub):
+def add_weight(thing, weight_sub, possible_solution):
     for x in structure[thing][1]:
         weight_sub[-1] += int(structure[x][0])
-        add_weight(x, weight_sub)
-    possible_solution = Counter(weight_sub)
+        add_weight(x, weight_sub, possible_solution)
+    if any(weight_sub[x] != weight_sub[0] for x in range(len(weight_sub))):
+        possible_solution = thing
     return weight_sub, possible_solution
 
-pillars, weight, possible_solution = create_pillars(solution)
+pillars, weight, possible_solution = create_pillars(solution, possible_solution)
 print(pillars)
 print(weight)
 
@@ -55,5 +54,8 @@ while len(set(weight)) == 2:
             index = weight.index(num)
             break
 
-    pillars, weight, possible_solution = create_pillars(pillars[index])
+    pillars, weight, possible_solution = create_pillars(pillars[index], possible_solution)
 print(possible_solution)
+for x in structure:
+    if possible_solution in structure[x][1]:
+        print([structure[y][0] for y in structure[x][1]])
