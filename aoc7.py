a60f3cc20774e62335b8ebecf264d743a38a8742
Ethan <ethanrodkin@gmail.com>
2017-12-07 01:23:18 -0500

    Ugly. Ran extra times w/ modified solution var to solve pt2. refactor tk

--- aoc7.py
+++ aoc7.py
@@ -7,15 +7,36 @@ test = []
 
 for program in file_:
     if len(program) == 2:
-        structure[(program[0], program[1])] = []
-        test.append(program[0])
+        structure[program[0]] = [int(program[1].strip('()')), []]
     elif len(program) > 2:
-        structure[(program[0], program[0])] = [program[x].strip(',') for x in range(3, len(program))]
+        structure[program[0]] = (int(program[1].strip('()')), [program[x].strip(',') for x in range(3, len(program))])
         for x in range(3, len(program)):
             test.append(program[x].strip(','))
 
 for program in file_:
     if len(program) > 2 and program[0] not in test:
         #pass
-        solution.append(program[0])
+        solution = program[0]
         print("part one: {}".format(program[0]))
+
+pillars = [x for x in structure[solution][1]]
+
+weight = []
+
+def add_weight(thing):
+    for x in structure[thing][1]:
+        weight[-1] += int(structure[x][0])
+        add_weight(x)
+
+for x in pillars:
+    weight.append(0)
+    check = weight[-1]
+    weight[-1] += structure[x][0]
+    # while weight[-1] != check:
+    #     check = weight[-1]
+    add_weight(x)
+        # for y in structure[x][1]:
+        #     weight[-1] += structure[y][0]
+
+print(weight)
+print(pillars)
