199a07d66117ba9e978c016f03aa3095588fb43b
Ethan <ethanrodkin@gmail.com>
2017-12-09 00:23:34 -0500

    Both solutions. Pretty simple. Rank 357 for pt 1 and 267 for pt 2.

---
+++ aoc9.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+with open('aoc9.txt') as f:
+    puzzle = str(line for line in f)
+
+end = len(puzzle)
+i = 0
+openings = []
+summary = Counter()
+biggest = 0
+garbage = False
+garbage_count = 0
+
+while i < end:
+    if puzzle[i] == '{' and garbage == False:
+        openings.append(1)
+        if len(openings) > biggest:
+            biggest = len(openings)
+    elif puzzle[i] == '}' and garbage == False:
+        summary[len(openings)] += 1
+        del(openings[-1])
+    elif puzzle[i] == '<' and garbage == False:
+        garbage = True
+    elif puzzle[i] == '>' and garbage == True:
+        garbage = False
+    elif garbage == True and puzzle[i] == '!':
+        i += 1
+    elif garbage == True:
+        garbage_count += 1
+    i += 1
+
+
+print("part one: {}".format(sum(summary[x] * x for x in summary)))
+print("part two: {}".format(garbage_count))
