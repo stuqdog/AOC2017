8debbc415690704e3fac094e8cf59113465a8849
Ethan <ethanrodkin@gmail.com>
2017-12-17 00:59:34 -0500

    Pt 1 almost instant, pt 2 takes ~14sec. There's gotta be a faster way.

---
+++ aoc17.py
@@ -0,0 +1,17 @@
+puzzle = [0]
+current = 0
+p_two = 0
+
+for x in range(1, 2018):
+    current = (current + 356) % x + 1
+    puzzle = puzzle[:(current)] + [x] + puzzle[(current):]
+
+solution = puzzle.index(2017) + 1
+print("Part one: {}".format(puzzle[solution]))
+
+for x in range(1, 50_000_001):
+    p_two = (p_two + 356) % x + 1
+    if p_two == 1:
+        solution = x
+
+print("Part two: {}".format(solution))
