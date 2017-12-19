b905f1efd801b5fc7e7fc1c2baa1d36f1131dcab
Ethan <ethanrodkin@gmail.com>
2017-12-19 10:36:19 -0500

    Removed needless checks. Come back to this one later, it can be better.

--- aoc19.py
+++ aoc19.py
@@ -1,3 +1,6 @@
+'''Note to self: come back to this one. Significant refactoring is def possible,
+would be a worthwhile project to figure out how to make this better.'''
+
 with open('aoc19.txt') as f:
     puzzle = [line for line in f]
 
@@ -7,8 +10,6 @@ solution_two = 0
 
 y = 0
 x = puzzle[0].index('|')
-y_max = len(puzzle)
-x_max = len(puzzle[0])
 solution_two = 0
 
 while True:
@@ -17,9 +18,9 @@ while True:
         solution += puzzle[y][x]
         
     if direction == 'down':
-        if (y+1) in range(y_max) and puzzle[y+1][x] != ' ':
+        if puzzle[y+1][x] != ' ':
             y += 1
-        elif (x+1) in range(x_max) and puzzle[y][x+1] != ' ':
+        elif puzzle[y][x+1] != ' ':
             x += 1
             direction = 'right'
         elif x > 0 and puzzle[y][x-1] != ' ':
@@ -31,7 +32,7 @@ while True:
     elif direction == 'left':
         if  x > 0 and puzzle[y][x-1] != ' ':
             x -= 1
-        elif (y+1) in range(y_max) and puzzle[y+1][x] != ' ':
+        elif puzzle[y+1][x] != ' ':
             y += 1
             direction = 'down'
         elif y > 0 and puzzle[y-1][x] != ' ':
@@ -43,7 +44,7 @@ while True:
     elif direction == 'up':
         if y > 0 and puzzle[y-1][x] != ' ':
             y -= 1
-        elif (x+1) in range(x_max) and puzzle[y][x+1] != ' ':
+        elif puzzle[y][x+1] != ' ':
             x += 1
             direction = 'right'
         elif x > 0 and puzzle[y][x-1] != ' ':
@@ -53,9 +54,9 @@ while True:
             break
 
     elif direction == 'right':
-        if (x+1) in range(x_max) and puzzle[y][x+1] != ' ':
+        if puzzle[y][x+1] != ' ':
             x += 1
-        elif (y+1) in range(y_max) and puzzle[y+1][x] != ' ':
+        elif puzzle[y+1][x] != ' ':
             y += 1
             direction = 'down'
         elif y > 0 and puzzle[y-1][x] != ' ':
