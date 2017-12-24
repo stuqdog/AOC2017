e582cf9f0f604af4ed2b306ef895df65bacade59
Ethan <ethanrodkin@gmail.com>
2017-12-24 11:03:22 -0500

    Refactor, small speed gain. Doesn't work if ports aren't sorted. Why?

--- aoc24.py
+++ aoc24.py
@@ -1,28 +1,30 @@
 with open("aoc24.txt") as f:
-    ports = [map(int, line.strip().split('/')) for line in f]
+    ports = [sorted(list(map(int, line.strip().split('/')))) for line in f]
 
-for i, line in enumerate(ports):
-    ports[i] = sorted(line)
+print(ports)
+# for i, line in enumerate(ports):
+#     ports[i] = sorted(line)
+# print(ports)
 
 solution1 = 0
 solution2 = 0
 solution2sum = 0
 
-ports = sorted(ports, key=lambda x: x[0])
-possible = [[x] for x in ports if 0 in x]
+possible = [[sorted(x)] for x in ports if 0 in x]
+print(possible)
 
 while possible:
     continues = False
     for port in ports:
-        if port[:] in possible[0] or port[::-1] in possible[0]:
+        if port in possible[0] or port[::-1] in possible[0]:
             pass
 
         elif port[0] == possible[0][-1][1]:
-            new = possible[0][:] + [port]
+            new = possible[0] + [port]
             possible.append(new)
             continues = True
         elif port[1] == possible[0][-1][1]:
-            new = possible[0][:] + [sorted(port, reverse=True)]
+            new = possible[0] + [sorted(port, reverse=True)]
             possible.append(new)
             continues = True
     if not continues:
@@ -30,7 +32,6 @@ while possible:
         for value in possible[0]:
             total += sum(x for x in value)
         if total > solution1:
-            print(total, solution2)
             solution1 = total
         if len(possible[0]) > solution2:
             solution2 = len(possible[0])
@@ -39,5 +40,5 @@ while possible:
             solution2sum = max(total, solution2sum)
     del possible[0]
 
-print(solution1)
-print(solution2sum)
+print("Part one:", solution1)
+print("Part two:", solution2sum)
